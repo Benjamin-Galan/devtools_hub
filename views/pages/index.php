76763e3b05bc827339b3e02989fae64557d706c4
Benja-Dev <begalan@outlook.es>
2024-10-20 15:16:15 -0600

    Cambiando de lugar el boton de explorar

--- views/pages/index.php
+++ views/pages/index.php
@@ -8,22 +8,23 @@
             <h1>Mejora y agiliza tu trabajo</h1>
             <p class="description">Explora una colección de herramientas en la web para impulsar tu
                 productividad, ya sea que desarrolles, diseñes o trabajes en el mundo digital.</p>
+
+            <div class="explore-container">
+                <div class="explore-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-rocket" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M4 13a8 8 0 0 1 7 7a6 6 0 0 0 3 -5a9 9 0 0 0 6 -8a3 3 0 0 0 -3 -3a9 9 0 0 0 -8 6a6 6 0 0 0 -5 3" />
+                        <path d="M7 14a6 6 0 0 0 -3 6a6 6 0 0 0 6 -3" />
+                        <path d="M15 9m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+                    </svg>
+                    <a href="/elements">Explorar</a>
+                </div>
+            </div>
         </div>
+
     </section>
 
     <?php include 'tools.php' ?>
-
-    <div class="explore-container">
-        <div class="explore-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-rocket" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M4 13a8 8 0 0 1 7 7a6 6 0 0 0 3 -5a9 9 0 0 0 6 -8a3 3 0 0 0 -3 -3a9 9 0 0 0 -8 6a6 6 0 0 0 -5 3" />
-                <path d="M7 14a6 6 0 0 0 -3 6a6 6 0 0 0 6 -3" />
-                <path d="M15 9m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-            </svg>
-            <a href="/elements">Explorar</a>
-        </div>
-    </div>
 
     <section class="about">
         <div class="about-text">
