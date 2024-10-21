182dc47b4624a5df15dec4363002914f1408e03e
Benja-Dev <begalan@outlook.es>
2024-10-20 20:23:36 -0600

    Eliminando clase blur para agregarla con javascript

--- views/pages/tools.php
+++ views/pages/tools.php
@@ -1,10 +1,10 @@
-<div id="toolsContainer" class="tools-grid blur">
+<div id="toolsContainer" class="tools-grid">
     <?php foreach ($tools as $tool) { ?>
         <div class="card" data-category-id="<?php echo $tool->category_id; ?>">
             <h3><?php echo $tool->name; ?></h3>
             <img class="border" src="/images/<?php echo $tool->image; ?>" alt="">
             <p><?php echo $tool->description; ?></p>
-            <a href="<?php echo $tool->url; ?>" class="button">Explorar</a>
+            <a href="<?php echo $tool->url; ?>" target="_blank"  class="button">Explorar</a>
         </div>
     <?php } ?>
 </div>
