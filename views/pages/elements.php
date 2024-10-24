6be6df4084f21f78e9a2961321869e3309c3eaeb
Benja-Dev <begalan@outlook.es>
2024-10-23 23:30:30 -0600

    Agregando clase para que el boton "todas" esté activo

--- views/pages/elements.php
+++ views/pages/elements.php
@@ -1,7 +1,7 @@
 <main class="container">
     <section class="elements"></section>
     <div class="tabs categories">
-        <button class="tab-button" data-category-id="0">Todas<a href="/tools.php"></a></button>
+        <button class="tab-button active" data-category-id="0">Todas<a href="/tools.php"></a></button>
         <?php foreach ($categories as $category) { ?>
             <button class="tab-button" data-category-id="<?php echo $category->id; ?>"><?php echo $category->name; ?></button>
         <?php } ?>
