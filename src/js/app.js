c2d51c662e3fe8db36ddb672915b4eed83982588
Benja-Dev <begalan@outlook.es>
2024-10-26 01:56:39 -0600

    Actualizando imagenes y comentarios

--- src/js/app.js
+++ src/js/app.js
@@ -11,37 +11,41 @@ document.addEventListener('DOMContentLoaded', function () {
 // Función asíncrona para obtener herramientas desde la API
 async function getToolsAPI() {
     try {
-        const URL = '/api/tools'; // URL de la API de herramientas
-        const result = await fetch(URL); // Realiza la solicitud
-        const tools = await result.json(); // Convierte la respuesta a JSON
-        listTools(tools); // Llama a la función para listar herramientas
+        const URL = '/api/tools'; 
+        const result = await fetch(URL); 
+        const tools = await result.json(); 
+        //Listar herramientas
+        listTools(tools); 
     } catch (error) {
-        console.log(error); // Maneja errores en la solicitud
+        console.log(error); 
     }
 }
 
 // Función asíncrona para obtener categorías desde la API
 async function getCategoriesAPI() {
     try {
-        const URL = '/api/category'; // URL de la API de categorías
-        const result = await fetch(URL); // Realiza la solicitud
-        const categories = await result.json(); // Convierte la respuesta a JSON
-        listCategories(categories); // Llama a la función para listar categorías
+        const URL = '/api/category'; 
+        const result = await fetch(URL); 
+        const categories = await result.json();
+        //Listar categorías
+        listCategories(categories); 
     } catch (error) {
-        console.log(error); // Maneja errores en la solicitud
+        console.log(error); 
     }
 }
 
 // Función para listar herramientas en el contenedor HTML
 function listTools(tools) {
-    const toolsContainer = document.querySelector('#toolsContainer'); // Selecciona el contenedor de herramientas
+    const toolsContainer = document.querySelector('#toolsContainer');
 
     tools.forEach(tool => {
-        const { name, image, description, url, category_id } = tool; // Desestructura los datos de la herramienta
+         //Obtener los datos de la herramienta
+        const { name, image, description, url, category_id } = tool;
 
-        const toolContainer = document.createElement('div'); // Crea un contenedor para la herramienta
-        toolContainer.classList.add('card'); // Añade la clase 'card' para estilos
-        toolContainer.setAttribute('data-category-id', category_id); // Almacena el ID de la categoría
+        //Contenedor para la herramienta
+        const toolContainer = document.createElement('div'); 
+        toolContainer.classList.add('card'); 
+        toolContainer.setAttribute('data-category-id', category_id); 
 
         // Crea y añade el nombre de la herramienta
         const toolName = document.createElement('h3');
@@ -49,19 +53,20 @@ function listTools(tools) {
 
         // Crea y añade la imagen de la herramienta
         const toolImg = document.createElement('IMG');
-        toolImg.loading = 'lazy'; // Activa carga diferida de la imagen
-        toolImg.src = `/images/${image}`; // Establece la fuente de la imagen
+        toolImg.loading = 'lazy'; 
+        toolImg.src = `/images/${image}`;
+        toolImg.alt = 'Imagen de la página';
 
         // Crea y añade la descripción de la herramienta
         const toolDescription = document.createElement('p');
-        toolDescription.textContent = description; // Cambiado a toolDescription
+        toolDescription.textContent = description;
 
         // Crea y añade el enlace a la herramienta
         const toolLink = document.createElement('a');
-        toolLink.href = url; // Establece el enlace
-        toolLink.textContent = 'Explorar'; // Texto del enlace
-        toolLink.target = '_blank'; // Abre el enlace en una nueva pestaña
-        toolLink.classList.add('button'); // Añade clase para estilos
+        toolLink.href = url; 
+        toolLink.textContent = 'Explorar'; 
+        toolLink.target = '_blank'; 
+        toolLink.classList.add('button');
 
         // Añade los elementos al contenedor de la herramienta
         toolContainer.appendChild(toolName);
@@ -76,49 +81,59 @@ function listTools(tools) {
 
 // Función para listar categorías en el contenedor HTML
 function listCategories(categories) {
-    const categoriesContainer = document.querySelector('#categories-container'); // Selecciona el contenedor de categorías
-    categoriesContainer.innerHTML = ''; // Limpia el contenido existente
+    const categoriesContainer = document.querySelector('#categories-container');
+    // Limpiar el contenido existente
+    categoriesContainer.innerHTML = ''; 
 
-    // Añade un botón para mostrar todas las categorías
+    // Botón para mostrar todas las categorías
     const allButton = createCategoryButton('Todas', 0);
     categoriesContainer.appendChild(allButton);
 
-    // Crea un botón para cada categoría y lo añade al contenedor
+    // Botón para cada categoría y lo añade al contenedor
     categories.forEach(category => {
         const { id, name } = category;
         const button = createCategoryButton(name, id);
         categoriesContainer.appendChild(button);
     });
 
-    // Establece el botón "Todas" como activo por defecto
+    // Establecer el botón "Todas" como activo por defecto
     allButton.classList.add('active');
-    filterTools(0); // Filtra las herramientas para mostrar todas
+
+    // Filtrar las herramientas para mostrar todas
+    filterTools(0); 
 }
 
 // Función para crear un botón de categoría
 function createCategoryButton(name, id) {
-    const button = document.createElement('BUTTON'); // Crea un botón
-    button.classList.add('tab-button'); // Añade clase para estilos
-    button.setAttribute('data-category-id', id); // Almacena el ID de la categoría
-    button.textContent = name; // Establece el texto del botón
+    const button = document.createElement('BUTTON'); 
+    button.classList.add('tab-button'); 
+    
+    // Almacena el ID y nombre de la categoría
+    button.setAttribute('data-category-id', id); 
+    button.textContent = name;
 
-    // Añade un evento para filtrar herramientas al hacer clic
+    //Evento para filtrar herramientas al hacer clic
     button.addEventListener('click', function() {
-        const allButtons = document.querySelectorAll('.tab-button'); // Selecciona todos los botones de categoría
-        allButtons.forEach(btn => btn.classList.remove('active')); // Remueve la clase 'active' de todos
-        button.classList.add('active'); // Establece 'active' en el botón clicado
-        filterTools(id); // Filtra herramientas por el ID de la categoría
+        const allButtons = document.querySelectorAll('.tab-button');
+        
+        //Remover la clase active y asignarla en función del click 
+        allButtons.forEach(btn => btn.classList.remove('active')); 
+        button.classList.add('active');
+
+        // Filtra herramientas por el ID de la categoría
+        filterTools(id); 
     });
 
-    return button; // Retorna el botón creado
+    return button;
 }
 
 // Función para filtrar las herramientas mostradas según la categoría seleccionada
 function filterTools(categoryId) {
-    const cards = document.querySelectorAll('#toolsContainer .card'); // Selecciona todas las tarjetas de herramientas
+    const cards = document.querySelectorAll('#toolsContainer .card');
     cards.forEach(card => {
-        const cardCategoryId = card.getAttribute('data-category-id'); // Obtiene el ID de la categoría de cada tarjeta
-        // Muestra la tarjeta si coincide con la categoría seleccionada o si se selecciona "Todas"
+        // Obtener el ID de la categoría de cada card
+        const cardCategoryId = card.getAttribute('data-category-id'); 
+        // Muestra la card si coincide con la categoría seleccionada o si se selecciona "Todas"
         if (categoryId == 0 || cardCategoryId == categoryId) {
             card.style.display = 'flex'; // Muestra la tarjeta
         } else {
@@ -127,6 +142,7 @@ function filterTools(categoryId) {
     });
 }
 
+//Desactiva el drop menu si se encuentra en la pantalla inicial
 function disabledDrop() {
     const header = document.querySelector('.header')
     const dropdown = document.querySelector('.nav .dropdown')
@@ -139,6 +155,7 @@ function disabledDrop() {
     }
 }
 
+//Desactiva el blur que se encuentra en la pantalla inicial
 function disableBlur() {
     const tools = document.querySelector('.tools-grid')
     const header = document.querySelector('.header')
@@ -150,17 +167,19 @@ function disableBlur() {
     }
 }
 
+//Llama al menu para dispositivos pequeños
 function mobileMenu() {
     const menu = document.querySelector('.menu');
     menu.addEventListener('click', showMenu)
 }
 
+//Oculta o muestra un menu en dispositivos pequeños
 function showMenu() {
     const navigation = document.querySelector('.nav')
     navigation.classList.toggle('show-menu')
 
     window.addEventListener('resize', () => {
-        if (window.innerWidth >= 768) { // Cambia este valor según tus necesidades
+        if (window.innerWidth >= 768) {
             navigation.classList.remove('show-menu'); // Oculta el menú en pantallas grandes
         } else {
             navigation.classList.remove('show-menu'); // También oculta en pantallas pequeñas por defecto
@@ -168,11 +187,13 @@ function showMenu() {
     });
 }
 
+//Menu dropdown para mostrar las categorias
 function dropDown() {
     const dropdown = document.querySelector('.dropdown')
     dropdown.addEventListener('click', showCategories)
 }
 
+//Mostrar las categorias
 function showCategories() {
     const categories = document.querySelector('.categories')
     const rotate = document.querySelector('.icon-tabler-chevron-down')
@@ -180,7 +201,7 @@ function showCategories() {
     rotate.classList.toggle('rotate')
 
     window.addEventListener('resize', () => {
-        if (window.innerWidth >= 768) { // Cambia este valor según tus necesidades
+        if (window.innerWidth >= 768) { 
             categories.classList.remove('show-list'); // Oculta el menú en pantallas grandes
         } else {
             categories.classList.remove('show-list'); // También oculta en pantallas pequeñas por defecto
